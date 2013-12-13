18db87f50750a2c376ce5a244f2bfd9395afbe52
kaleksandrov <kiril.aleksandrov.89@gmail.com>
2013-12-13 17:38:44 +0200

    Added socket example project.

---
+++ SocketExample/src/bg/tusofia/pik3/main/SocketExample.java
@@ -0,0 +1,17 @@
+package bg.tusofia.pik3.main;
+
+/**
+ * A program that shows a basic usage of sockets
+ * 
+ * @author kaleksandrov
+ * 
+ */
+public class SocketExample {
+
+	/**
+	 * The entry point of the program
+	 */
+	public static void main(String[] args) {
+
+	}
+}
